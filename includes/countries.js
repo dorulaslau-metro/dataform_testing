54775be712c791e07627a22106b9f092bacf43db
Doru Laslau <doru.laslau@metro-external.digital>
2025-12-10 13:12:06 +0000

    Kept only MD and BG for first test run

--- includes/countries.js
+++ includes/countries.js
@@ -1,11 +1,11 @@
 module.exports.countries = [
     { iso2: "BG", internal: "BUL"},
-    { iso2: "CZ", internal: "CZE"},
+    // { iso2: "CZ", internal: "CZE"},
     // { iso2: "ES", internal: "ESP"},
     // { iso2: "FR", internal: "FRA"},
     // { iso2: "HR", internal: "CRO"},
     // { iso2: "HU", internal: "HUN"},
-    // { iso2: "MD", internal: "MDA"},
+    { iso2: "MD", internal: "MDA"},
     
 //... add the rest of the countries in this list
 ];
