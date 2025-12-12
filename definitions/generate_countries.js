cda78acb5f2a094d5b07dde8c56af43c52082028
Doru Laslau <doru.laslau@metro-external.digital>
2025-12-12 12:50:19 +0000

    changed "bbd_operational2_1_XX" to "bbd_operational2_1_daily_XX" to emulate the current logic in dataset "Country_dashboard"

--- definitions/generate_countries.js
+++ definitions/generate_countries.js
@@ -22,7 +22,7 @@ countries.forEach((c) => {
         }
     }).query(bbd_operational1_XX(c));
 
-        publish(`bbd_operational2_1_${c.iso2}`, {
+        publish(`bbd_operational2_1_daily_${c.iso2}`, {
         type: "table", 
         schema: "temp_orchestration",
         bigquery: {
@@ -39,7 +39,7 @@ countries.forEach((c) => {
             //requirePartitionFilter: true
         },
         // ensure dependency on bbd_operational1_XX and bbd_operational2_1_XX
-        dependencies: [`bbd_operational1_${c.iso2}`, `bbd_operational2_1_${c.iso2}`]
+        dependencies: [`bbd_operational1_${c.iso2}`, `bbd_operational2_1_daily_${c.iso2}`]
     }).query(bbd_operational2_XX(c));
 
 
